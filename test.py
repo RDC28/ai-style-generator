c9c93c2262265621096262b77c7fbc925c013ba5
Rohit Chavda <119717311+RDC28@users.noreply.github.com>
2025-09-08 22:12:32 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyDKkoG4kmMcgTt_CviOZH7lT1BARoeysAk")
-
-models = client.models.list()
-print("Models supporting generateContent:")
-for m in models:
-    if "generateContent" in m.supported_actions:
-        print(m.name)
